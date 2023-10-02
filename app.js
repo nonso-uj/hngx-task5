ad1a60564ca7803398a3bcaba6d7c79615fa8135
nonso-uj <nonso.udonne@gmail.com>
2023-10-02 10:17:57 +0100

    commit 2

--- app.js
+++ app.js
@@ -60,21 +60,26 @@ app.get('/', (req, res) => {
         res.status(400).json({error: "Requires Range header"})
     }
     const videoPath = "./uploads/video-Kingpin-is-Fat-(Laugh)-1696205575909.mp4"
-    const videoSize = fs.statSync(videoPath).size
-    const CHUNK_SIZE = 10 ** 6
-    const start = Number(range.replace(/\D/g, ""))
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
+    try{
+        const videoSize = fs.statSync(videoPath).size
+        const CHUNK_SIZE = 10 ** 6
+        const start = Number(range.replace(/\D/g, ""))
+        const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+        const contentLength = end - start + 1;
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            "Content-Type": "video/mp4",
+        }
+    
+        res.writeHead(206, headers)
+        const videoStream = fs.createReadStream(videoPath, {start, end})
+        videoStream.pipe(res)
+    }catch(err){
+        console.log(err)
+        res.status(400).json({error: err.message})
     }
-
-    res.writeHead(206, headers)
-    const videoStream = fs.createReadStream(videoPath, {start, end})
-    videoStream.pipe(res)
 })
 
 
